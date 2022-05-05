37e878c28f70878b05e5541df90562029d647bcb
DESKTOP-JU60D4G\lizekun <imu.lizekun@gmail.com>
2022-05-06 01:15:16 +0800

    调通test

--- ptsr/data/benchmark.py
+++ ptsr/data/benchmark.py
@@ -18,11 +18,15 @@ class Benchmark(srdata.SRData):
     def _set_filesystem(self, dir_data):
 
         self.apath = os.path.join(dir_data, 'sr_test')
-        print("self.scale = ", self.scale)
+        # print("self.scale = ", self.scale)
+        print("{}: scale = {}".format(self.name, self.scale))
+        # self.dir_hr = os.path.join(
+        #     self.apath, "HR/{}/x{}".format(self.name, self.scale[0]))
         self.dir_hr = os.path.join(
-            self.apath, "HR/{}/x{}".format(self.name, self.scale[0]))
+            self.apath, "{}/HR".format(self.name))
 
-        self.dir_lr = os.path.join(self.apath,  "LRBI/{}/".format(self.name))
+        # self.dir_lr = os.path.join(self.apath,  "LRBI/{}/".format(self.name))
+        self.dir_lr = os.path.join(self.apath, "{}/LR_bicubic/X{}".format(self.name, self.scale[0]))
         self.ext = ('.png', '.png')
 
     def _scan(self):
@@ -33,12 +37,11 @@ class Benchmark(srdata.SRData):
         names_lr = [[] for _ in self.scale]
         for f in names_hr:
             filename, _ = os.path.splitext(os.path.basename(f))
-            filename = filename.replace("_HR_x{}".format(self.scale[0]), "")
+            # filename = filename.replace("_HR_x{}".format(self.scale[0]), "")
             for si, s in enumerate(self.scale):
                 names_lr[si].append(os.path.join(
-                    self.dir_lr, 'x{}/{}_LRBI_x{}{}'.format(
-                        s, filename, s, self.ext[1]
-                    )
+                    # self.dir_lr, 'x{}/{}_LRBI_x{}{}'.format(s, filename, s, self.ext[1])
+                    self.dir_lr, '{}x{}{}'.format(filename, s, self.ext[1])
                 ))
 
         return names_hr, names_lr
